5987f1400a87edd26161f16595c42f6ca22ef0b5
totalbot123 <milanjecmenica@gmail.com>
2021-03-24 07:48:48 -0400

    Modified test script

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,7 @@
 
 APP_PATH=$HOME/monitoring
 
-docker run --rm -ti $JOB_NAME:$BUILD_NUMBER-SIT pytest tests/ > unit.txt
+docker run --name=$JOB_NAME --rm -ti $JOB_NAME:$BUILD_NUMBER-SIT pytest tests/ > unit.txt
 FAIL=$(cat unit.txt | grep "fail")
 
 cat unit.txt
@@ -11,6 +11,7 @@ if [[ $FAIL != "" ]]; then
   exit 1
 fi
 
+docker stop $JOB_NAME 1>/dev/null && docker rm $JOB_NAME 1>/dev/null
 docker run --name=$JOB_NAME -p 8000:80 -d $JOB_NAME:$BUILD_NUMBER-SIT
 
 sleep 3
